57ca86bd6efdb05c8477b93822de3601bd73f1a5
scruffyscarf <scruffyscarf@mail.ru>
2025-07-02 01:24:12 +0300

    lab02: implement frontend and backend tasks
    - Implement chatcore.go, message.go, and user.go in backend folde
    - Implement chat_screen.dart, chat_service.dart, main.dart, user_profile.dart, and user_service.dart in frontend folder
    - Add counter_app.dart

--- labs/lab02/backend/chatcore/chatcore.go
+++ labs/lab02/backend/chatcore/chatcore.go
@@ -6,7 +6,6 @@ import (
 	"sync"
 )
 
-// Message represents a chat message
 type Message struct {
 	Sender    string
 	Recipient string
@@ -37,6 +36,11 @@ func (b *Broker) Run() {
 		select {
 		case <-b.ctx.Done():
 			close(b.done)
+			b.usersMutex.Lock()
+			for _, ch := range b.users {
+				close(ch)
+			}
+			b.usersMutex.Unlock()
 			return
 		case msg := <-b.input:
 			b.usersMutex.RLock()
@@ -63,10 +67,17 @@ func (b *Broker) SendMessage(msg Message) error {
 	}
 }
 
-func (b *Broker) RegisterUser(userID string, recv chan Message) {
+func (b *Broker) RegisterUser(userID string, recv chan Message) error {
+	select {
+	case <-b.ctx.Done():
+		return errors.New("broker context canceled")
+	default:
+	}
+
 	b.usersMutex.Lock()
 	defer b.usersMutex.Unlock()
 	b.users[userID] = recv
+	return nil
 }
 
 func (b *Broker) UnregisterUser(userID string) {
